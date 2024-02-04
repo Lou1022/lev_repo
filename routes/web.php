07fe24285797f605b5157e4e8e2c050369e4c618
Lou1022 <dejon_mcyyy@yahoo.co.jp>
2024-02-04 11:20:51 +0900

    New page update

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () { return view('welcome'); });
 
 Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
